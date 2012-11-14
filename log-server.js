4183beafda5e2ad5e932f7f43719ce81f79e33a2
Joo Antonio Ferreira <joao.parana@gmail.com>
2012-11-14 13:42:05 -0200

    acertos

--- log-server.js
+++ log-server.js
@@ -2,36 +2,68 @@ var app = require('http').createServer(handler);
 var io = require('socket.io').listen(app);
 var fs = require('fs');
 var static = require('node-static');
-  
 
-/* http://www.sitepoint.com/serving-static-files-with-node-js  */
+/* http://www.sitepoint.com/serving-static-files-with-node-js */
 var webroot = ".";
 var options = {
-  cache: 600,
-  headers: { 'X-Powered-By': 'node-static' }
+  cache : 600,
+  headers : {
+    'X-Powered-By' : 'node-static'
+  }
 };
 var fileServer = new static.Server(webroot, options);
 
 // Ver o que será necessário fazer de refactory abaixo
 app.listen(80);
 
-function handler (req, res) {
+io.configure('development', function() { // can be 'production' too
+  io.set('log level', 2); // reduce logging to INFO level
+  // io.set('transports', [ 'websocket', 'flashsocket', 'htmlfile',
+  // 'xhr-polling', 'jsonp-polling' ]);
+});
+
+function handler(req, res) {
   var resource = '.' + req.url;
   console.log('Carregando : ' + resource);
-  fs.readFile(__dirname + req.url,
-  function (err, data) {
+  fs.readFile(__dirname + req.url, function(err, data) {
     if (err) {
       res.writeHead(500);
-      return res.end('Error loading ' + resource + ' from "' + (__dirname + req.url) + '"');
+      return res.end('Error loading ' + resource + ' from "'
+          + (__dirname + req.url) + '"');
     }
     res.writeHead(200);
     res.end(data);
   });
 }
 
-io.sockets.on('connection', function (socket) {
-  socket.emit('news-from-server', { hello: 'world' });
-  socket.on('log-event', function (data) {
-    console.log(data);
+var logMsg = '';
+
+function transverse(myObject, func) {
+  var ret = null;
+  for ( var prop in myObject) {
+    // to filter out prototype properties
+    if (myObject.hasOwnProperty(prop)) {
+      ret = func('\n' + prop + ': ', myObject[prop]);
+    }
+  }
+  logMsg = '';
+  return ret;
+}
+
+function formatLog(prop, msg) {
+  return logMsg + msg;
+}
+
+io.sockets.on('connection', function(socket) {
+  // https://github.com/LearnBoost/Socket.IO/wiki/Configuring-Socket.IO
+
+  socket.emit('news-from-server', {
+    hello : 'world'
+  });
+  console.log('---  New Connection established ' + (new Date()).toString()
+      + ' ---');
+  console.log('Waiting for "log-event" from client');
+  socket.on('log-event', function(data) {
+    console.log('LOG_FROM_CLIENT ' + transverse(data, formatLog));
   });
 });
